26d10dd4a9d93bad9c7638b5f5864cf80c3bef2f
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-06 02:47:57 +0200

    Update the application controller file to configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name, :email, :password) }
+    # devise_parameter_sanitizer.permit(:confirmation) { |u| u.permit(:email, :password) }
+    # devise_parameter_sanitizer.permit(:password) { |u| u.permit(:email, :password) }
+  end
 end
